ebf0577caafb1e88e6ede6a5363477f3cc165ce0
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2014-12-19 13:59:46 +0000

    New operation: add an edge in a finite graph.

--- Graphs/FGraphs.v
+++ Graphs/FGraphs.v
@@ -7,8 +7,24 @@ Record fgraph := mk_fgraph {
   fg_edges : list (fg_t * fg_t) % type
 }.
 
+Implicit Arguments mk_fgraph.
+
 (* Typed version of finite graphs *)
 Definition fgraph_t (V:Type) := {x : fgraph | fg_t x = V }.
+
+Definition fg_t_unpack {V:Type} (g:fgraph_t V) : fgraph := 
+  match g with
+    | exist a b => a
+  end.
+
+Lemma fg_t_prop:
+  forall {V:Type} (g:fgraph_t V),
+  fg_t (fg_t_unpack g) = V.
+Proof.
+  intros.
+  destruct g.
+  auto.
+Qed.
 
 Definition fg_t_edges {V:Type} (g:fgraph_t V) : (list (V * V) % type).
 Proof.
@@ -16,6 +32,13 @@ Proof.
   assert (Y:= fg_edges x).
   subst.
   assumption.
+Defined.
+
+Definition mk_fgraph_t {V:Type} (es:list (V*V)%type) : fgraph_t V.
+  set (g := mk_fgraph es).
+  unfold fgraph_t.
+  apply exist with (x:=g).
+  auto.
 Defined.
 
 Definition EqDec (V:Type) := forall (v1 v2:V), {v1 = v2} + {v1 <> v2}.
@@ -29,8 +52,8 @@ Axiom all_pos_odegree_impl_cycle:
   forall {V:Type} g,
   EqDec V ->
   fg_t_edges g <> nil ->
-  (forall v1, In v1 g -> exists v2, Edge g (v1, v2)) ->
-  exists w, Cycle V (Edge g) w.
+  (forall v1, In v1 g -> exists (v2:V), Edge g (v1, v2)) ->
+  exists w, Cycle (Edge g) w.
 
 Require Import Lists.ListSet.
 Print ListSet.
@@ -98,8 +121,8 @@ Qed.
 Lemma walk_subgraph:
   forall {V:Type} (eq_dec: EqDec V) (g g':fgraph_t V) w,
   subgraph eq_dec g g' ->
-  Walk V (Edge g) w ->
-  Walk V (Edge g') w.
+  Walk (Edge g) w ->
+  Walk (Edge g') w.
 Proof.
   intros.
   assert (forall_w: List.Forall (Edge g') w).
@@ -119,8 +142,8 @@ Qed.
 Lemma cycle_subgraph:
   forall {V:Type} (eq_dec: EqDec V) (g g':fgraph_t V) w,
   subgraph eq_dec g g' ->
-  Cycle V (Edge g) w ->
-  Cycle V (Edge g') w.
+  Cycle (Edge g) w ->
+  Cycle (Edge g') w.
 Proof.
   intros.
   inversion H0.
@@ -130,3 +153,53 @@ Proof.
   apply walk_subgraph with (eq_dec0:=eq_dec) (g0:=g); repeat auto.
 Qed.
 
+Definition add {V:Type} (e:(V*V)%type) (g:fgraph_t V) : (fgraph_t V) :=
+  mk_fgraph_t (cons e (fg_t_edges g)).
+
+Lemma fg_edges_mk:
+  forall {V:Type} e,
+  fg_t_edges (@mk_fgraph_t V e) = e.
+Proof.
+  intros.
+  remember (mk_fgraph_t e) as g.
+  unfold fgraph_t in *.
+  inversion g.
+  unfold mk_fgraph_t in *.
+  subst.
+  unfold fg_t_edges.
+  auto.
+Qed.
+
+Lemma add_edges:
+  forall {V} e g,
+  fg_t_edges (add (V:=V) e g) =
+  cons e (fg_t_edges g).
+Proof.
+  intros.
+  unfold add.
+  rewrite fg_edges_mk.
+  auto.
+Qed.
+
+Lemma add_le:
+  forall {V:Type} (eq_dec: EqDec V) (g:fgraph_t V) e,
+  subgraph eq_dec g (add e g).
+Proof.
+  intros.
+  unfold subgraph.
+  unfold set_le.
+  intros.
+  rewrite add_edges.
+  apply List.in_cons.
+  assumption.
+Qed.
+
+Lemma cycle_add:
+  forall {V:Type} (eq_dec: EqDec V) (g:fgraph_t V) e w,
+  Cycle (Edge g) w ->
+  Cycle (Edge (add e g)) w.
+Proof.
+  intros.
+  assert (sub := add_le eq_dec g e).
+  apply cycle_subgraph with (eq_dec0:=eq_dec) (g0:=g); repeat assumption.
+Qed.

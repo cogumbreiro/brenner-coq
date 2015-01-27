25d65394b233a10053b11dec097c1bf40110d452
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-01-27 12:06:35 -0100

    Added utility methods for resources.

--- Vars.v
+++ Vars.v
@@ -5,11 +5,13 @@ Require Import Coq.FSets.FMapAVL.
 Require Coq.FSets.FMapFacts.
 Require Coq.FSets.FSetProperties.
 Require Import MapUtil.
+Require Import SetUtil.
 
 Module TID := Nat_as_OT.
 Module TID_Facts := OrderedTypeFacts TID.
 Module Set_TID := FSetAVL.Make TID.
 Module Set_TID_Props := FSetProperties.Properties Set_TID.
+Module Set_TID_Extra := SetUtil Set_TID.
 Module Map_TID := FMapAVL.Make TID.
 Module Map_TID_Facts := FMapFacts.Facts Map_TID.
 Module Map_TID_Props := FMapFacts.Properties Map_TID.
@@ -20,6 +22,7 @@ Definition set_tid := Set_TID.t.
 
 Module PHID := Nat_as_OT.
 Module Set_PHID := FSetAVL.Make PHID.
+Module Set_PHID_Extra := SetUtil Set_PHID.
 Module Map_PHID := FMapAVL.Make PHID.
 Module Map_PHID_Facts := FMapFacts.Facts Map_PHID.
 Module Map_PHID_Props := FMapFacts.Properties Map_PHID.
